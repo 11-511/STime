86d04427d68ac5c9468c386db2ad10c372e1bd6f
Miles Tong <1761619776@qq.com>
2019-06-04 23:02:30 +0800

    测试完成

--- app/src/main/java/xyz/miles/stime/activity/MainActivity.java
+++ app/src/main/java/xyz/miles/stime/activity/MainActivity.java
@@ -19,15 +19,15 @@ import xyz.miles.stime.R;
 
 public class MainActivity extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		Bmob.initialize(this, "782ebc87bf1c101e8c607d7e6bf17a31");
-		
-		
+
+
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		FloatingActionButton fab = findViewById(R.id.fab);
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity
 		toggle.syncState();
 		navigationView.setNavigationItemSelectedListener(this);
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -56,49 +56,49 @@ public class MainActivity extends AppCompatActivity
 			super.onBackPressed();
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		
+
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@SuppressWarnings("StatementWithEmptyBody")
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {
 		// Handle navigation view item clicks here.
 		int id = item.getItemId();
-		
+
 		if (id == R.id.nav_home) {
 			// Handle the camera action
 		} else if (id == R.id.nav_my_image) {
-		
+
 		} else if (id == R.id.nav_collections) {
-		
+
 		} else if (id == R.id.nav_subscribe) {
-		
+
 		} else if (id == R.id.nav_logout) {
-		
+
 		} else if (id == R.id.nav_exit) {
-		
+
 		}
-		
+
 		DrawerLayout drawer = findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
--- app/src/main/java/xyz/miles/stime/activity/SignUpActivity.java
+++ app/src/main/java/xyz/miles/stime/activity/SignUpActivity.java
@@ -1,40 +1,91 @@
 package xyz.miles.stime.activity;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 import xyz.miles.stime.R;
+import xyz.miles.stime.bean.STimeComment;
+import xyz.miles.stime.bean.STimePicture;
+import xyz.miles.stime.bean.STimeUser;
 
 public class SignUpActivity extends AppCompatActivity {
-	
-	
-	
-	
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sign_up);
-		
-		EditText editText_name=findViewById(R.id.et_account_o);
-		EditText editText_pwd=findViewById(R.id.et_pwd_o);
-		EditText editText_nick=findViewById(R.id.et_pwd_s);
-		
-		String name=editText_name.getText().toString();
-		String pwd=editText_pwd.getText().toString();
-		String nick=editText_nick.getText().toString();
-		
-		Button bt=findViewById(R.id.bt_sign_up);
-		bt.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
-			}
-		});
-		
-	}
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        Bmob.initialize(this, "782ebc87bf1c101e8c607d7e6bf17a31");
+
+        EditText editText_name = findViewById(R.id.et_account_o);
+        EditText editText_pwd = findViewById(R.id.et_pwd_o);
+        EditText editText_nick = findViewById(R.id.et_pwd_s);
+
+        final String name = editText_name.getText().toString();
+        final String pwd = editText_pwd.getText().toString();
+        final String nick = editText_nick.getText().toString();
+
+        Button bt = findViewById(R.id.bt_sign_up);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//				STimeUser user=new STimeUser();
+//				user.setNickname("2");
+//				user.setUsername("2");
+//				user.setPassword("2");
+//				user.signUp(new SaveListener<STimeUser>() {
+//
+//					@Override
+//					public void done(STimeUser o, BmobException e) {
+//						if (e == null) {
+//							Toast.makeText(SignUpActivity.this,"注册成功",Toast.LENGTH_LONG);
+//						} else {
+//							Toast.makeText(SignUpActivity.this,"注册失败",Toast.LENGTH_LONG);
+//						}
+//					}
+//				});
+
+//				STimeComment comment=new STimeComment();
+//				comment.setCommentContent("asfsaf");
+//				comment.save(new SaveListener<String>() {
+//					@Override
+//					public void done(String objectId,BmobException e) {
+//						if(e==null){
+//							Toast.makeText(SignUpActivity.this,"注册成功",Toast.LENGTH_LONG).show();
+//						}else{
+//							Toast.makeText(SignUpActivity.this,"注册失败",Toast.LENGTH_LONG).show();
+//						}
+//					}
+//				});
+
+
+                STimePicture user = new STimePicture();
+                user.setPictureTitle("title");
+                user.setPictureBrief("brief");
+                user.save(new SaveListener<String>() {
+
+                    @Override
+					public void done(String objectId,BmobException e) {
+						if(e==null){
+							Toast.makeText(SignUpActivity.this,"注册成功",Toast.LENGTH_LONG).show();
+						}else{
+							Toast.makeText(SignUpActivity.this,"注册失败",Toast.LENGTH_LONG).show();
+						}
+					}
+                });
+
+            }
+        });
+
+    }
 }
